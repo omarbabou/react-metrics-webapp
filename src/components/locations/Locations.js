6ad7bbd905803d4e5c43522026a3e0725c340172
omarbabou <obabou50@gmail.com>
2022-08-31 11:40:20 +0200

    Add locations functionality

--- src/components/locations/Locations.js
+++ src/components/locations/Locations.js
@@ -1,7 +1,62 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { IoIosArrowForward, IoIosPin } from 'react-icons/io';
+import { BiSearchAlt2 } from 'react-icons/bi';
+import { v4 as uuid } from 'uuid';
+import { getData } from '../../redux/reducer/reducers';
+import weathe from '../../graphics/mweathe.png';
 
-const Locations = () => (
-  <div> Locations</div>
-);
+const Locations = () => {
+  const [searchCapital, setSearchCapital] = React.useState('');
+  const dispatch = useDispatch();
+  const handleChange = (event) => {
+    setSearchCapital(event.target.value);
+  };
+  const CountryCapitols = [
+    'Kigali', 'Kampala', 'Nairobi', 'Caire', 'Kinshasa',
+    'Pretoria', 'Abuja', 'Dakar', 'Bamako', 'Rabat', 'Tunis',
+    'Accra', 'Luanda', 'Libreville', 'Bangui', 'Djouba', 'Alger',
+    'Conakry', 'Lusaka', 'Moroni', 'Mogadishu', 'Praia', 'Gaborone', 'Harare', 'Tripoli',
+  ];
+  const searchedCountriesCapitols = searchCapital.trim().length
+   > 0 ? CountryCapitols.filter((c) => c.toLowerCase().includes(searchCapital.toLowerCase()))
+    : CountryCapitols;
+
+  return (
+    <>
+      <div className="container">
+        <div className="location-table">
+          <div className="location-input">
+            <input type="loc-text" onChange={handleChange} value={searchCapital} id="input-data" placeholder="search here" />
+            <BiSearchAlt2 id="search" />
+          </div>
+          <div className="capitols-table">
+
+            {searchedCountriesCapitols.map((loc) => (
+              <Link className="link" key={uuid()} to="/details">
+                <div
+                  className="location-town"
+                  id={loc}
+                  onClick={() => { dispatch(getData(`${loc}`)); }}
+                  role="button"
+                  tabIndex={0}
+                  onKeyDown={() => { dispatch(getData(`${loc}`)); }}
+                >
+                  <div className="my-link"><IoIosArrowForward className="loc-arrow" /></div>
+                  <img src={weathe} alt="weather-icons" />
+                  <div className="loc-city">
+                    <IoIosPin />
+                    {loc}
+                  </div>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Locations;
