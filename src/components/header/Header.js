c9036cd936df7ac8e49e051541c20f81b33ce376
omarbabou <obabou50@gmail.com>
2022-08-31 10:52:01 +0200

    Add Link to the header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { BiMicrophone } from 'react-icons/bi';
+import { BsFillGearFill } from 'react-icons/bs';
+import { IoIosArrowBack } from 'react-icons/io';
+import { Link } from 'react-router-dom';
 
 const Header = () => (
-  <div> Header page</div>
+    <div className="Header">
+    <Link to="/">
+      <IoIosArrowBack className="header-back" />
+    </Link>
+    <div className="header-logo">Look at your weather!</div>
+    <div className="header-icons">
+      <BiMicrophone />
+      <BsFillGearFill />
+    </div>
+  </div>
 );
 
 export default Header;
