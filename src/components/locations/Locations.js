7a2ca98153b0be92556ea345432a17d2dacc068f
omarbabou <obabou50@gmail.com>
2022-09-01 15:34:04 +0200

    Add cities

--- src/components/locations/Locations.js
+++ src/components/locations/Locations.js
@@ -13,7 +13,7 @@ const Locations = () => {
   const handleChange = (event) => {
     setSearchQuery(event.target.value);
   };
-  const CountryCapitols = ['Kinshasa', 'Matadi', 'Kisangani', 'Tanganyika', 'Lualaba', 'Kolwezi', 'Lodja', 'Businga', 'Fizi', 'Uvira', 'Kamituga', 'Beni', 'Butembo', 'Bunia', 'Kalemie', 'Ituri', 'Boende', 'Bosobolo', 'Kabinda', 'Boma', 'Gbadolite', 'Likasi', 'Gemena', 'Bandundu', 'kananga'];
+  const CountryCapitols = ['Kinshasa', 'Matadi', 'Kisangani', 'Tanganyika', 'Lualaba', 'Kolwezi', 'Lodja', 'Businga', 'Fizi', 'Uvira', 'Kamituga', 'Beni', 'Butembo', 'Bunia', 'Kalemie', 'Ituri', 'Boende', 'Luebo', 'Kabinda', 'Moanda', 'Gbadolite', 'Likasi', 'Gemena', 'Bandundu', 'kananga'];
   const searchedCountriesCapitols = searchQuery.trim().length
    > 0 ? CountryCapitols.filter((c) => c.toLowerCase().includes(searchQuery.toLowerCase()))
     : CountryCapitols;
